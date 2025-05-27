670e3cc53e40fbc8f3368323f84f8f3b3cb25878
Michel Arriaga <victor_arriaga@alfaware.com.mx>
2025-05-27 13:34:44 -0600

    refactor: cambio a useReducer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
-import type { CartItem, Guitar } from "../types"
+import { Dispatch, useMemo } from "react"
+import type { CartItem } from "../types"
+import { CartActions } from "../reducers/cart-reducer"
 
 type HeaderProps = {
     cart: CartItem[]
-    removeFromCart: (id: Guitar['id']) => void
-    decreaseQuantity: (id: Guitar['id']) => void
-    increaseQuantity: (id: Guitar['id']) => void
-    clearCart: () => void
-    isEmpty: boolean
-    cartTotal: number
+    dispatch: Dispatch<CartActions>
 }
 
-export default function Header({ cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } : HeaderProps) {
+export default function Header({ cart, dispatch} : HeaderProps) {
+    //state derivado
+    const isEmpty = useMemo(() => cart.length === 0, [cart])
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.price * item.quantity),0), [cart])
     return (
         <header className="py-5 header">
             <div className="container-xl">
@@ -55,7 +55,7 @@ export default function Header({ cart, removeFromCart, decreaseQuantity, increas
                                                             <button
                                                                 type="button"
                                                                 className="btn btn-dark"
-                                                                onClick={() => decreaseQuantity(guitar.id)}
+                                                                onClick={() => dispatch({type: 'decrease-quantity', payload: {id: guitar.id}})}
                                                             >
                                                                 -
                                                             </button>
@@ -63,7 +63,7 @@ export default function Header({ cart, removeFromCart, decreaseQuantity, increas
                                                             <button
                                                                 type="button"
                                                                 className="btn btn-dark"
-                                                                onClick={() => increaseQuantity(guitar.id)}
+                                                                onClick={() => dispatch({type: 'increase-quantity', payload: {id: guitar.id}})}
                                                             >
                                                                 +
                                                             </button>
@@ -72,7 +72,7 @@ export default function Header({ cart, removeFromCart, decreaseQuantity, increas
                                                             <button
                                                                 className="btn btn-danger"
                                                                 type="button"
-                                                                onClick={() => removeFromCart(guitar.id)}
+                                                                onClick={() => dispatch({type: 'remove-from-cart', payload: {id: guitar.id}})}
                                                             >
                                                                 X
                                                             </button>
@@ -84,7 +84,7 @@ export default function Header({ cart, removeFromCart, decreaseQuantity, increas
                                         <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                     </>
                                 )}
-                                <button className="btn btn-dark w-100 mt-3 p-2" onClick={clearCart}>Vaciar Carrito</button>
+                                <button className="btn btn-dark w-100 mt-3 p-2" onClick={() => dispatch({type: 'clear-cart'})}>Vaciar Carrito</button>
                             </div>
                         </div>
                     </nav>
